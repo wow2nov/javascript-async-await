9e3c870ea169d37419c1888aac38057e0f26be67
Supapond <supapondmaliwab@gmail.com>
2025-10-30 15:28:21 +0700

    fead:add code ex-3

--- ex-3.js
+++ ex-3.js
@@ -33,3 +33,11 @@ let getJohnOrders = () => {
 };
 
 // Start coding here
+getJohnProfile()
+.then((profile) => {console.log(profile)})
+.catch((error) => {console.log("Error", error)});
+
+getJohnOrders()
+.then((profile) => {console.log(profile)})
+.catch((error) => {console.log("Error", error)})
+
